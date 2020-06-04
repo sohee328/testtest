0652d216de5586653e6fb9cbf0630759ac064423
goomokhyun <kmh0324x@naver.com>
2020-06-04 16:32:58 +0900

    testdd

--- src/GOOMOKHYUN.js
+++ src/GOOMOKHYUN.js
@@ -4,7 +4,7 @@ class soheeTest extends Component {
     render() {
         return (
             <div>
-                <b>goomokhdfjkl</b>
+                <b>test1632</b>
             </div>
         );
     }
