8d0fa6cd6116a462fb076ce261df3e412d982d31
SoHee <sohee960328@naver.com>
2020-06-04 12:51:35 +0900

    soheeTest

---
+++ src/soheeTest.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+class soheeTest extends Component {
+    render() {
+        return (
+            <div>
+                <b>박소희</b>
+            </div>
+        );
+    }
+}
+
+export default soheeTest;
