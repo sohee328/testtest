c121a39de4488e94858a494fa20a829a34bcfe98
goomokhyun <kmh0324x@naver.com>
2020-06-04 16:27:54 +0900

    1627 test

---
+++ src/GOOMOKHYUN.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+class soheeTest extends Component {
+    render() {
+        return (
+            <div>
+                <b>goomokhdfjkl</b>
+            </div>
+        );
+    }
+}
+
+export default soheeTest;
