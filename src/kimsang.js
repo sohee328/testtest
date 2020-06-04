073fd59aa6b1f2c6cccb59c9f4bc28430420ae66
sanghoon <tommyksh123@gmail.com>
2020-06-04 16:20:46 +0900

    VS테

---
+++ src/kimsang.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+class soheeTest extends Component {
+    render() {
+        return (
+            <div>
+                <b>김상</b>
+            </div>
+        );
+    }
+}
+
+export default soheeTest;
